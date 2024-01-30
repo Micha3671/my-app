5e696cd84dfd3dbb81d3e3b44771452c0d0cf331
Micha <michael.rigas@t-online.de>
2024-01-30 10:58:38 +0100

    Navigationsbar.js berichtigt

--- src/Navigationsbar.js
+++ src/Navigationsbar.js
@@ -1,15 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-  );
+  return <nav></nav>;
 }
 
 export default Navbar;
